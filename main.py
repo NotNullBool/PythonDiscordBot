dc80f8da38c45b24abfeed80f6a92773ddabcdc7
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2022-08-06 19:49:01 +0000

    token handler start

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 """ Discord Bot Application
 """
 import discord
+import token_handler
 
 class MyClient(discord.Client):
+    """MyClient The Base Client
+
+    Args:
+        discord (discord.Client): The base client info for bot
+    """
     async def on_ready(self):
-        print(f"Logged on as {self.user}")
-    
+        """on_ready when bot first starts
+        """
+        print(f"Logged on as {self.user}")       
     async def on_message(self, message: discord.Message):
+        """on_message when message is sent in discord
+
+        Args:
+            message (discord.Message): message info
+        """
         print(f"Message from {message.author}: {message.content}")
 
 client = MyClient()
 
-client.run()
+client.run(token_handler.get_token())
---
+++ token_handler.py
@@ -0,0 +1,10 @@
+""" Handler for bot token
+"""
+import json
+
+# TODO: try load token, except create token json alert terminal
+def get_token() -> str:
+    try:
+        pass
+    except:
+        pass

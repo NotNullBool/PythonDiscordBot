03f06d5bf0890b3328db9a6cc594e1b04d88fc47
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2022-08-07 01:59:48 +0000

    Token verification setup

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """ Discord Bot Application
 """
 import discord
-import token_handler
+import meta_info_handler
+
+TOKEN: str = meta_info_handler.get_token()
 
 class MyClient(discord.Client):
     """MyClient The Base Client
@@ -12,7 +14,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         """on_ready when bot first starts
         """
-        print(f"Logged on as {self.user}")       
+        print(f"Logged on as {self.user}")
     async def on_message(self, message: discord.Message):
         """on_message when message is sent in discord
 
@@ -21,6 +23,9 @@ class MyClient(discord.Client):
         """
         print(f"Message from {message.author}: {message.content}")
 
-client = MyClient()
-
-client.run(token_handler.get_token())
+if __name__ == "__main__":
+    client = MyClient()
+    try:
+        client.run(TOKEN)
+    except discord.LoginFailure:
+        print("token.json needs a valid token")
---
+++ meta_info_handler.py
@@ -0,0 +1,25 @@
+""" Handler for bot meta info
+"""
+import json
+from typing import Dict
+from discord import LoginFailure
+
+# TODO: try load token, except create token json alert terminal
+
+def get_token(token_file_path: str = "./token.json") -> str:
+    """get_token Get Discord Bot Token
+    Returns:
+        str: Token
+    """
+    token_dict: Dict ={"token":""}
+    try:
+        with open(token_file_path, 'r', encoding= "utf-8") as file_:
+            token_dict = json.load(file_)
+    except IOError as err:
+        print(f"Exception: {err}, token.json does not yet exist")
+        with open(token_file_path, 'w', encoding= "utf-8") as file_:
+            json.dump(token_dict, file_)
+    finally:
+        if token_dict["token"] == "":
+            raise LoginFailure(f"token at {token_file_path} needs to be set")
+    return token_dict["token"]
